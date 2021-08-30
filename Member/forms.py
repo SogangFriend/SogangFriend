8dc71e406a1d3476105d4dee04874852a3ac34a8
mysoo <80390638+mysoo@users.noreply.github.com>
2021-08-30 20:29:52 +0900

    email activate
    21.08.30

--- Member/forms.py
+++ Member/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class LoginForm(forms.Form):
     email = forms.CharField(label="Email", max_length=100)
     password = forms.CharField(label="Password", widget=forms.PasswordInput(), required=True)
+
--- Member/models.py
+++ Member/models.py
@@ -11,6 +11,12 @@ class Member(models.Model):
     password = models.CharField(max_length=30, blank=False)
     # 알아봐야될거같은데 보안 때문에, 사용자가 패스워드로 123456 입력 -> 내부에 해쉬함수로 못알아보는 값으로 저장을 해야 돼요 -> 장고는 어떻게?
     introduction = models.TextField(blank=True, null=True)  # help_text(필드입력도움말기능) -> 폼에서 설정합니당!
+    # is_active = models.BooleanField(default=False)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_active = models.BooleanField(
+        'active',
+        default=False,  #이메일 인증 전까지는 False인 상태
+        help_text="")
 
     def __str__(self):
         return self.name
