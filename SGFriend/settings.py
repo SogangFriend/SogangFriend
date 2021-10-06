b41c4d2f95de04b31b3fcfc5fa3da272e65a5fe4
dls4585 <dls4585@naver.com>
2021-10-06 15:53:26 +0900

    Update settings.py

--- SGFriend/settings.py
+++ SGFriend/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = './mainApp/templates/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, './mainApp/templates/static/temp')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mainApp/templates/static/')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
