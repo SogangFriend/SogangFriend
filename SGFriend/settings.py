e5393e2285dc2f34fc2873d617e07275112262af
dls4585 <dls4585@naver.com>
2021-10-06 16:55:04 +0900

    Update settings.py

--- SGFriend/settings.py
+++ SGFriend/settings.py
@@ -15,7 +15,7 @@ import os
 from .my_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve()
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
