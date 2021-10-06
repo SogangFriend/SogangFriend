ce7cd25458ca13b943b6cffd25eac006e05b1998
dls4585 <dls4585@naver.com>
2021-10-06 16:29:40 +0900

    move static

--- SGFriend/settings.py
+++ SGFriend/settings.py
@@ -15,7 +15,7 @@ import os
 from .my_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve()
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
