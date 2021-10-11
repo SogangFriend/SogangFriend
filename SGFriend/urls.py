1fda6d3a13af295fe020bb2732d63d23082678de
dls4585 <dls4585@naver.com>
2021-10-11 22:27:55 +0900

    urls

--- SGFriend/urls.py
+++ SGFriend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainApp.urls')),
+    path('', include('mainApp.urls', namespace='Main')),
     path('member/', include('Member.urls', namespace='Member')),
     path('chat/', include('chat.urls', namespace='Chat')),
 
--- mainApp/urls.py
+++ mainApp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from .views import *
 from Member import views
 
+app_name ='Main'
+
 urlpatterns = [
     path('', HomeView.as_view(), name="homepage"),
     path('members/', MemberListView.as_view()),
