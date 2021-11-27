2d91f1767874c21f65bdc65f2f20f88bceac5282
dls4585 <dls4585@naver.com>
2021-11-28 01:26:05 +0900

    Fix : url 2

--- Chat/urls.py
+++ Chat/urls.py
@@ -5,7 +5,7 @@ app_name = 'Chat'
 urlpatterns = [
     path('', ChatView.as_view(), name='room'),
     path('new/', RoomCreateView.as_view(), name='chat_create'),
-    path('/<int:room_pk>/', EnterChatView.as_view(), name='list'),
+    path('/<int:room>/', EnterChatView.as_view(), name='enter'),
     path('dm/<str:pk>/', EnterDMView.as_view(), name='dm'),
 
     # ajax 통신 url
